4db08ad928dc89a92a44abe44ca31b829b83a1c6
Prabhat Chandela <chandela.prabhat@gmail.com>
2024-03-09 23:12:43 +0530

    appwrite databaseService setup

---
+++ src/appwrite/bucketService.js
@@ -0,0 +1,9 @@
+import { Client, Account, ID } from "appwrite";
+
+export class BucketService {
+
+}
+
+const bucketService = new BucketService();
+export default bucketService;
+
---
+++ src/appwrite/databaseService.js
@@ -0,0 +1,75 @@
+import { Client, Databases, ID } from "appwrite";
+import config from "../config/config";
+
+export class DatabaseService {
+    client = new Client();
+    databases;
+
+    constructor() {
+        this.client.setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+
+        this.databases = new Databases(this.client);
+    }
+
+    async createPost({ caption, location, featuredimage, status, userId }) {
+        try {
+            return await this.databases.createDocument(config.appwriteDatabaseId, config.appwriteCollectionId, ID.unique(),
+                {
+                    caption,
+                    location,
+                    featuredimage,
+                    status,
+                    userId
+                })
+
+        } catch (error) {
+            console.log("Appwrite::createPost::error::", error);
+        }
+    }
+
+    async deletePost(postId) {
+        try {
+            await this.databases.deleteDocument(config.appwriteDatabaseId, config.appwriteCollectionId, postId);
+            return true;
+        } catch (error) {
+            console.log("Appwrite::deletePost::error::", error);
+            return false;
+        }
+    }
+
+    async updatePost(postId , { caption, location, featuredimage, status, userId }) {
+        try {
+            return await this.databases.updateDocument(config.appwriteDatabaseId, config.appwriteCollectionId, postId,
+                {
+                    caption,
+                    location,
+                    featuredimage,
+                    status,
+                    userId
+                }
+            );
+        } catch (error) {
+            console.log("Appwrite::updatePost::error::", error);
+        }
+    }
+
+    async getPost(postId){
+        try {
+            return await this.databases.getDocument(config.appwriteDatabaseId, config.appwriteCollectionId, postId);
+        } catch (error) {
+            console.log("Appwrite::getPost::error::", error);
+        }
+    }
+
+    async getPosts(){
+        try {
+           return await this.databases.listDocuments(config.appwriteDatabaseId, config.appwriteCollectionId);
+        } catch (error) {
+            console.log("Appwrite::getPosts::error::", error);
+        }
+    }
+}
+
+const databaseService = new DatabaseService();
+export default databaseService;
